541e1950470de60524d92c09831bf592fa4dd8b3
p.kiris <p.kiris@141.19.145.78>
2014-11-18 14:28:24 +0100

    AssoziativesArray

--- TPE3/src/hs_mannheim/ws14/tpe_uib_05/ueb3/AssoziativesArray.java
+++ TPE3/src/hs_mannheim/ws14/tpe_uib_05/ueb3/AssoziativesArray.java
@@ -1,5 +1,20 @@
 package hs_mannheim.ws14.tpe_uib_05.ueb3;
 
-public class AssoziativesArray {
-//geistesgestörte pinar :*
+public interface AssoziativesArray {
+	
+	public void clear ();
+	public int containsValue ();
+	public int containsKey ();
+	public int get ();
+	public int isEmpty ();
+	public void put ();
+	public void putAll ();
+	public void remove ();
+	public int size ();
+	public void update ();
+	public void forEach ();
+	
+
+
+
 }
