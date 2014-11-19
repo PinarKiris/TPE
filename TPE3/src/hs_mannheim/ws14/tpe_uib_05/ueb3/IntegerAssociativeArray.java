868a8f0ac8221b688802cceb5eae9c2dcce55b57
p.kiris <p.kiris@141.19.145.69>
2014-11-19 15:40:16 +0100

    Klassen

---
+++ TPE3/src/hs_mannheim/ws14/tpe_uib_05/ueb3/IntegerAssociativeArray.java
@@ -0,0 +1,83 @@
+package hs_mannheim.ws14.tpe_uib_05.ueb3;
+
+public class IntegerAssociativeArray implements AssoziativesArray{
+
+	@Override
+	public void clear() {
+		// TODO Automatisch generierter Methodenstub
+		
+	}
+
+	@Override
+	public int containsValue() {
+		// TODO Automatisch generierter Methodenstub
+		return 0;
+	}
+
+	@Override
+	public int containsKey() {
+		// TODO Automatisch generierter Methodenstub
+		return 0;
+	}
+
+	@Override
+	public int get() {
+		// TODO Automatisch generierter Methodenstub
+		return 0;
+	}
+
+	@Override
+	public int isEmpty() {
+		// TODO Automatisch generierter Methodenstub
+		return 0;
+	}
+
+	@Override
+	public void put() {
+		// TODO Automatisch generierter Methodenstub
+		
+	}
+
+	@Override
+	public void putAll() {
+		// TODO Automatisch generierter Methodenstub
+		
+	}
+
+	@Override
+	public int remove() {
+		// TODO Automatisch generierter Methodenstub
+		return 0;
+	}
+
+	@Override
+	public int size() {
+		// TODO Automatisch generierter Methodenstub
+		return 0;
+	}
+
+	@Override
+	public void update() {
+		// TODO Automatisch generierter Methodenstub
+		
+	}
+
+	@Override
+	public void forEach() {
+		// TODO Automatisch generierter Methodenstub
+		
+	}
+
+	@Override
+	public void extractAll() {
+		// TODO Automatisch generierter Methodenstub
+		
+	}
+
+	@Override
+	public void map() {
+		// TODO Automatisch generierter Methodenstub
+		
+	}
+
+}
