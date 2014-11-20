6ca28f564a5f102fc21aacd7fdb5d40514d7f6b7
PinarKiris <pinar.kiris@hotmail.de>
2014-11-20 09:45:40 +0100

    Baumknoten

---
+++ TPE3/src/hs_mannheim/ws14/tpe_uib_05/ueb3/Baumknoten.java
@@ -0,0 +1,17 @@
+package hs_mannheim.ws14.tpe_uib_05.ueb3;
+
+public class Baumknoten <T> {
+	private T links;
+	private T rechts;
+	
+	public Baumknoten (T links, T rechts){
+		this.links = links;
+		this.rechts = rechts;
+	}
+	
+	public String toString (){
+		return this.links + "," + this.rechts;
+	}
+	
+
+}
