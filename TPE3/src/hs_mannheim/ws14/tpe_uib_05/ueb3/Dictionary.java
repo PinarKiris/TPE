252d63edb001e7086c7994ba95190e2169724579
Crystallize5000 <eraser5000@gmx.de>
2014-11-24 19:04:55 +0100

    Dictionary + fixes

---
+++ TPE3/src/hs_mannheim/ws14/tpe_uib_05/ueb3/Dictionary.java
@@ -0,0 +1,92 @@
+package hs_mannheim.ws14.tpe_uib_05.ueb3;
+
+public class Dictionary implements AssoziativesArray {
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean containsValue(Object value) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsKey(Object key) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int get(Object key) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void put() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void putAll() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int remove() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void update(Object value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void forEach() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void extractAll() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void map() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void keys() {
+		
+	}
+	
+	public void values () {
+		
+	}
+	
+
+}
