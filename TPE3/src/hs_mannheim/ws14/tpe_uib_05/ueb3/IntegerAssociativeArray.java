69f65367cd37820a091bbfcf83dbe69401b24a31
Crystallize5000 <eraser5000@gmx.de>
2014-11-26 20:19:34 +0100

    bit Javadoc + Vererbung

--- TPE3/src/hs_mannheim/ws14/tpe_uib_05/ueb3/IntegerAssociativeArray.java
+++ TPE3/src/hs_mannheim/ws14/tpe_uib_05/ueb3/IntegerAssociativeArray.java
@@ -1,9 +1,19 @@
 package hs_mannheim.ws14.tpe_uib_05.ueb3;
 
+/**
+ * Diese Klasse ist von der Klasse "typesafeAssociativeArray" abgeleitet und
+ * arbeitet diesbezueglich ebenfalls mit einem binären Suchbaum, der jedoch nur
+ * Integer-Werte verwaltet.
+ * 
+ * @author Dennis Keßler 1326697
+ * @author Pınar Kırış 1332722
+ * 
+ * @param <K>
+ *            Typ Parameter des Schluessels
+ *
+ */
 
-
-public class IntegerAssociativeArray  {
-	
-
+public class IntegerAssociativeArray<K> extends
+		typesafeAssociativeArray<K, Integer> {
 
 }
--- TPE3/src/hs_mannheim/ws14/tpe_uib_05/ueb3/StringAssociativeArray.java
+++ TPE3/src/hs_mannheim/ws14/tpe_uib_05/ueb3/StringAssociativeArray.java
@@ -1,9 +1,19 @@
 package hs_mannheim.ws14.tpe_uib_05.ueb3;
 
+/**
+ * Diese Klasse ist von der Klasse "typesafeAssociativeArray" abgeleitet und
+ * arbeitet diesbezueglich ebenfalls mit einem binären Suchbaum, der jedoch nur
+ * String Schluessel verwaltet.
+ * 
+ * @author Dennis Keßler 1326697
+ * @author Pınar Kırış 1332722
+ * 
+ * @param <V>
+ *            Typ Parameter des Wertes
+ *
+ */
 
-
-public class StringAssociativeArray {
-
-	
+public class StringAssociativeArray<V> extends
+		typesafeAssociativeArray<String, V> {
 
 }
